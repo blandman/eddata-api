e4e30a629935343cc204f1dbc3c1ea7f54d10460
Kris Hagel <kris@hatrackgroup.com>
2015-03-26 13:24:56 -0700

    Added sections model and route to add sections to the api.

---
+++ app/models/sections.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var timestamps = require('mongoose-timestamp');
+var autoIncrement = require('mongoose-auto-increment');
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var SectionSchema = new Schema({
+  psId: Number,
+  buildingStateCode: Number,
+  courseName: String,
+  expression: String,
+  sectionNumber: Number,
+  courseNumber: String,
+  term: Number,
+  studentCount: Number,
+  room: String,
+  maxEnrollment: Number,
+  courseCode: String,
+  teacher: {
+    staffID: Number
+  },
+  firstDay: Date,
+  lastDay: Date,
+  refreshAccount: Boolean
+});
+
+SectionSchema.plugin(timestamps);
+SectionSchema.plugin(autoIncrement.plugin, { model: 'Sections', field: 'id' });
+SectionSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('Section', SectionSchema);
---
+++ app/routes/sections.js
@@ -0,0 +1,183 @@
+var Section = require('../models/sections');
+var express = require('express');
+var router = express.Router();
+var utils = require('../utils');
+var async = require('async');
+var passport = require('passport');
+
+router.route('/v1/sections')
+
+  .post(passport.authenticate('bearer', { session: false }),function(req, res) {
+    var section = new Section(req.body.section);
+    section.save(function (err, obj) {
+      if(err) 
+        res.send(err);
+      obj._id = undefined;
+      obj.__v = undefined;
+      var data = {
+        "section": obj,
+        "meta": {
+          "href": process.env.API_URL + '/api/v1/sections/' + obj.id
+        }
+      }
+      res.json(data);
+    });
+  })
+
+  .put(passport.authenticate('bearer', { session: false }),function(req, res) {
+    Section.findOne({psId: req.body.section.psId}, function (err, sct) {
+      if (sct) {
+        if (sct.expression == req.body.section.expression && sct.studentCount == req.body.section.studentCount 
+        && sct.room == req.body.section.room && sct.teacher.staffId == req.body.section.teacher.staffId) {
+          req.body.section.refreshAccount = false;
+        } else {
+          req.body.section.refreshAccount = true;
+        }
+        var now = new Date().getTime();
+        req.body.section.updatedAt = now;
+        Section.findOneAndUpdate({psId: req.body.section.psId}, {$set: req.body.section}, function(err,section) {
+          if (err) 
+            res.send(err);
+          if (section) {
+            section._id = undefined;
+            section.__v = undefined;
+            var data = {
+              "section": section,
+              "meta": {
+                "href": process.env.API_URL + '/api/v1/sections/' + sct.id
+              }
+            }
+            res.send(data);
+          } else {
+            res.send(404,JSON.stringify({"error": "sectionNotFound"}));
+          }
+        });                  
+      } else {
+        req.body.section.refreshAccount = true;
+        var section = new Section(req.body.section);
+        section.save(function (err, obj) {
+          if(err) 
+            res.send(err);
+          obj._id = undefined;
+          obj.__v = undefined;
+          var data = {
+            "section": obj,
+            "meta": {
+              "href": process.env.API_URL + '/api/v1/sections/' + obj.id
+            }
+          }
+          res.json(data);
+        });
+      }
+    });
+  })
+
+  .get(passport.authenticate('bearer', { session: false }),function(req, res) {
+    var qString = req.query;
+    if (qString.ids) {
+      var queryOne = Section.find({ id: { $in: qString.ids } });
+      var queryTwo = Section.find({ id: { $in: qString.ids } });
+    } else if (qString.staffid) {
+      var queryOne = Section.find({ teacher.staffId: qString.staffid });
+      var queryTwo = Section.find({ teacher.staffId: qString.staffid });
+    } else if (qString.bldg) {
+      var queryOne = Section.find({ buildingStateCode: qString.bldg });
+      var queryTwo = Section.find({ buildingStateCode: qString.bldg });
+    } else if (qString.refresh) {
+      var queryOne = Section.find({ refreshAccount: qString.refresh });
+      var queryTwo = Section.find({ refreshAccount: qString.refresh });
+    } else {
+      var queryOne = Section.find({});
+      var queryTwo = Section.find({});
+    }
+    var limit = utils.setLimit(req.query.limit);
+    var offset = utils.setOffset(req.query.offset);
+    async.parallel({
+      count: function(callback){
+        queryOne.count(function(err, count) {
+          callback(null, count);
+        });
+      },
+      records: function(callback){
+        queryTwo.skip(offset).select('-_id -__v -salt -hash').limit(limit).exec('find', function(err, items) {
+          callback(null, items);
+        });
+      }
+    },
+    function(err, results) {
+      if(err)
+        res.send(err);
+      if (results) {
+        var next = utils.pageNext(offset,limit,results.count,'sections');
+        var prev = utils.pagePrev(offset,limit,results.count,'sections');
+        var data = {
+          "section": results.records,
+          "meta": {
+            "total": results.count,
+            "offset": offset,
+            "limit": limit,
+            "href": process.env.API_URL + '/api/v1/sections?offset=' + offset + '&limit=' + limit,
+            "next": next,
+            "previous": prev
+          }
+        }
+        res.json(data);
+      } else {
+        res.send(404,JSON.stringify({"error": "sectionsNotFound"}));
+      }
+    });
+  })
+
+router.route('/v1/sections/:id')
+
+  .get(passport.authenticate('bearer', { session: false }),function(req, res) {
+    Section.findOne({id: req.params.id}, function(err, obj) {
+      if (err)
+        res.send(err);
+      if (obj) {
+        obj._id = undefined;
+        obj.__v = undefined;
+        var data = {
+          "section": obj,
+          "meta": {
+            "href": process.env.API_URL + '/api/v1/sections/' + req.params.id
+          }
+        }
+        res.json(data);
+      } else {
+        res.send(404,JSON.stringify({"error": "sectionNotFound"}));
+      }
+    });
+  })
+
+  .put(passport.authenticate('bearer', { session: false }),function(req, res) {
+    var now = new Date().getTime();
+    req.body.section.updatedAt = now;
+    Section.findOneAndUpdate({id: req.params.id}, {$set: req.body.section}, function(err,section) {
+      if (err) 
+        res.send(err);
+      if (section) {
+        section._id = undefined;
+        section.__v = undefined;
+        var data = {
+          "section": section,
+          "meta": {
+            "href": process.env.API_URL + '/api/v1/sections/' + req.params.id
+          }
+        }
+        res.send(data);
+      } else {
+        res.send(404,JSON.stringify({"error": "sectionNotFound"}));
+      }
+    });
+  })
+
+  .delete(passport.authenticate('bearer', { session: false }),function(req, res) {
+    Section.remove({id: req.params.id}, function(err, section) {
+      if (err)
+        res.send(err);
+      res.json(section);
+    });
+  });
+
+module.exports = router;
