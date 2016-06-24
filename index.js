41b2b31304bb90aa1a2fb309a5722adff608ec7b
MARTIN <martinb@dttsd405254m114.peninsula.wednet.edu>
2016-06-24 09:50:21 -0700

    No changes to index

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var winston = require('winston');
 var autoIncrement = require('mongoose-auto-increment');
 var passport = require('passport');
 
+
 //variables for use in winston logging
 var myLogTransports = [];
 myLogTransports.push(new (winston.transports.Console)({ json: true, colorize: true, dumpExceptions: true, showStack: true, timestamp: true }));
