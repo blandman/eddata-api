652c0a2ef42ae81fed63f1039d39569636971269
brandon martin <programmer.brandon@gmail.com>
2017-07-03 14:16:00 -0700

    Update rooms.js
    Added groundFloor, speakers, and computers mapping for building rooms.

--- app/models/rooms.js
+++ app/models/rooms.js
@@ -19,7 +19,11 @@ var RoomSchema = new Schema({
 	building: {type: Number, ref: "Building"},
 
 	name: String,
-	floor: Number
+	floor: Number,
+	groundFloow: Number,
+	
+	speakers: [],
+	computers: []
 });
 
 RoomSchema.plugin(timestamps);
